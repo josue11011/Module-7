04cb2d49a4820a430a10b1c71f87fb6e028bcb44
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-11-19 19:41:50 -0600

    Update flower.py

--- flower.py
+++ flower.py
@@ -3,10 +3,12 @@
 # Date: November 12, 2022
 # Purpose of a Program: To create a flower
 import turtle
-def drawSquare(t,sz):
+def drawSquare(t,sz): #change name as either polygon or hexagon
     for i in range(6):
         t.forward(90)
-        t.left(300)
+        #t.left(300)
+        t.left(60)
+        
 
 
 wn = turtle.Screen()
@@ -14,15 +16,18 @@ wn = turtle.Screen()
 alex = turtle.Turtle()
 alex.color("pink")
 
-t = alex
+#t = alex
 sz = 20
 for i in range(10):
-   t._rotate(45)
-   alex.pensize(3)
-   drawSquare(t,sz)
-   sz = sz + 20
-   alex.penup()
-   alex.goto(alex.pos() + (-10, -10))
-   alex.pendown()
+   drawSquare(alex, sz)
+   alex.right(36)
+
+   #alex._rotate(45)
+   #alex.pensize(3)
+   #drawSquare(alex,sz)
+   #sz = sz + 20
+   #alex.penup()
+   #alex.goto(alex.pos() + (-10, -10))
+   #alex.pendown()
 wn.mainloop()
 wn.exitonclick()
