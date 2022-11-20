1db15f30c6a90b9f98c3f32f8f537407c4b0cc30
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-11-19 19:28:29 -0600

    Update CheckRange.py

--- CheckRange.py
+++ CheckRange.py
@@ -5,13 +5,28 @@
 
 import random
 
-def CheckRange():
-    n = int(input("What is your number?"))
-    if n in range(1,10):
+def CheckRange(num): #a nubmer is passed to check its range
+    #n = int(input("What is your number?"))
+    if n in range(1,11): #upper bound exclusive
         print("This number is in thr range 1-10.")
+        return True
     else:
         print("This number is not in the range 1-10")
-CheckRange()
+        return False
+    
+def main():
+    count  = 0 : #variable to count if the passed number is in range.
+    for i in range(20):
+        num = random.randint(1, 15) #upperbound inclusive
+        if CheckRange(num) == True:
+            count += 1
+        else:
+            pass
+     print("The random numbers are in the given range for {} times".format(count))
+ main()
+
+'''
+Not recommend
 def twenty_numlist():
     print(random.randrange(1,15))
     print(random.randrange(1, 15))
@@ -34,6 +49,6 @@ def twenty_numlist():
     print(random.randrange(1, 15))
     print(random.randrange(1, 15))
 twenty_numlist()
-
+'''
 
 
